ec9e53e1596fea2ae969ac3da66d6196bfc62fea
Benedikt Magnus <magnus@magnuscraft.de>
2023-11-21 11:56:08 +0100

    Updated Laszlo.

--- langs/laszlo/compile.sh
+++ langs/laszlo/compile.sh
@@ -3,7 +3,7 @@ gcc --version
 if [ "$(uname -sm)" = "Linux x86_64" ]; then
 	git clone https://github.com/mgerhold/laszlo.git
 	cd laszlo
-	git checkout 930603b8d0f803dd80dba864535fff69d0f89cba
+	git checkout 138bd989a1fe6128cbed53ecd8a890370f85ecff
 	cmake .
 	make
 	cd ..
